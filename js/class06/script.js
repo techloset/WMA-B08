c94ce31142f158f3168fbf0bf8ce4c5d76216655
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-01-04 20:46:13 +0500

    Array and Functions class

---
+++ js/class06/script.js
@@ -0,0 +1,60 @@
+
+// var name1 = "Ali";
+// var name3 = "Zain"
+// var name4 = "Umar"
+
+var a = 10;
+a = 20;
+var b = 10;
+
+var nums = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Ali", "Zain", "Umar", "Naveed", "Ali", 20, false, NaN, undefined, null];
+
+
+
+
+
+// var result = Math.random() * 10;
+
+// List basic builin functions in javascript
+// 1. alert()
+// 2. console.log()
+// 3. prompt()
+// 4. confirm()
+// 5. setTimeout()
+// 6. setInterval()
+
+
+
+
+// num1 = num1.toString();
+// var result = Math.pow(2,3);
+// var result = Math.min(1,2,3,4,5,6,7,8,9,10);
+// var result = Math.max(1,2,3,4,5,6,7,8,9,10);
+// var result = Math.random() * 100;
+// result = Math.ceil(7.3);
+// result = Math.floor(7.6);
+// result = Math.round(7.6);
+// var output = "2.3"
+// output = parseInt(output);
+// output = parseFloat(output);
+// console.log(output);
+
+
+
+
+// function mulitply(c,d){
+//     return c * d;
+// }
+
+// function add(...args){
+//     return args
+//     return d;
+//     console.log("c",c);
+//     console.log("d",d);
+// }
+
+
+// var result = add(2,3);
+
+// console.log("result",result);
