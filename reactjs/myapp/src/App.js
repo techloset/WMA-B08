4c6010a974dd8a69de0dc7528ae38183c92f2a46
devnaveedsarwar <learnwithnaveedsarwar@gmail.com>
2025-03-22 09:04:40 +0500

    new contact

--- reactjs/myapp/src/App.js
+++ reactjs/myapp/src/App.js
@@ -1,34 +1,56 @@
-import { BrowserRouter,Routes, Route, useLocation  } from "react-router";
+import {useState} from 'react'
 
-import Home from './pages/home/Home';
-import About from './pages/about/About';
-import Contact from './pages/contact/Contact';
-import Services from './pages/services/Services';
-import Button from "./components/button/Button";
-import Header from "./components/header/Header";
-
-
-function App() {
-
-
-
+export default function App() {
+  const [userName, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
+  const [contacts, setContacts] = useState([])
+ 
+  const handleClick = () => {
+    console.log('Name:', userName)
+    console.log('Email:', email)
+    console.log('Phone:', phone)
+    console.log('Password:', password
+    )
+    const contact = {
+      name: userName,
+      email: email,
+      phone: phone,
+      password: password
+    }
+    setContacts([...contacts, contact])
+  }
+  
+  
   return (
     <div>
+      <h1>Contact Form</h1>
 
-    <BrowserRouter>
+      <input type="text" onChange={(e)=>setName(e.target.value)} placeholder='Enter Name' />
+      <br/>
+      <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Email'/>
+      <br/>
+      <input type="text" onChange={(e)=>setPhone(e.target.value)} placeholder='Enter Phone' />
+      <br/>
+      <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Enter Password' />
+      <br/>
+      <button onClick={handleClick}>Submit</button>
 
-    <Header/>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/services" element={<Services />} />
-    <Route path="*" element={<h1>404 Not Found</h1>} />
-    </Routes>  
-    </BrowserRouter>
-
-
+      <hr/>
+      <h2>Contacts</h2>
+      <ul>
+        {contacts.length === 0 && <li>No contacts available</li>}
+        {contacts.map((contact, index) => (
+          <li key={index}>
+            <p>Name: {contact.name}</p>
+            <p>Email: {contact.email}</p>
+            <p>Phone: {contact.phone}</p>
+            <p>Password: {contact.password}</p>
+          </li>
+        ))}
+      </ul>
+      
     </div>
   )
 }
-export default App;
